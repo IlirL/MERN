43fd9a1118995936463c2045730e9806d615a3e2
IlirL <ilir.limani@live.com>
2021-02-24 13:48:54 +0100

    add tiktok clone backend

---
+++ tiktok-clone/backend/data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+         url:"https://media.w3.org/2010/05/sintel/trailer_hd.mp4",
+         channel:"Ilir'sChannel",
+         song:"Lead to the river",
+         likes:"2133",
+         messages:"3125524",
+         description: "This is a description",
+         shares: "3123234",
+    },
+    {
+        url:"https://media.w3.org/2010/05/sintel/trailer.ogv",
+        channel:"Ilir'sChannel",
+        song:"Lead to the river",
+        likes:"2133",
+        messages:"3125524",
+        description: "This is a description",
+        shares: "3123234",
+   }
+]
+
+export default data;
